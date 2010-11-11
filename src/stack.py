3f706b3903bb06a5eea23980afd67c91a4d0136d
nurettin senyer <nurettin.senyer@bil.omu.edu.tr>
2010-11-11 14:13:13 +0200

    peek/pop guncellendi + print destegi eklendi

--- src/stack.py
+++ src/stack.py
@@ -1,19 +1,35 @@
 class Stack():
-    def __init__(self):
-        self.items=[]
-    def isEmpty(self):
-        return self.items==[]
-    def push(self,item):
-        self.items.append(item)
-    def pop(self,n=None):
-        if n==None:
-             return self.items.pop()
-        else:
-             return self.items.pop(n)
-    def peek(self,n=None):
-        if n==None:
-             return self.items[len(self.items)-1]
-        else:
-             return self.items[n]
-    def size(self):
-        return len(self.items)
+	def __init__(self):
+		self.items=[]
+
+	def isEmpty(self):
+		return self.items==[]
+
+	def push(self,item):
+		self.items.append(item)
+
+	def pop(self,n=None):
+		if self.isEmpty():
+			return None
+		if n==None:
+			return self.items.pop()
+		else:
+			return self.items.pop(n)
+
+	def peek(self,n=None):
+		if self.isEmpty():
+			return None
+		if n==None:
+			return self.items[len(self.items)-1]
+		else:
+			return self.items[n]
+
+	def size(self):
+		return len(self.items)
+
+	def print(self, title=""):
+		print title,
+		for i in self.items:
+			print i,
+		print
+
